bc9d01eb97f7c8d43c76f25344948459762dc794
Brian <brian@brianthe.dev>
2022-06-27 16:26:22 +0700

    Use executemany; generate db on build

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN pip install -r requirements.txt
 
 COPY . /app
 
+RUN python update.py
+
 CMD ["gunicorn", "-k", "geventwebsocket.gunicorn.workers.GeventWebSocketWorker", "-b", "0.0.0.0:5000", "-w", "1", "app:app"]
--- update.py
+++ update.py
@@ -1,21 +1,34 @@
-import sqlite3, requests
+import sqlite3
+import requests
 
+# Crete db if it doesn't exist
 with sqlite3.connect('tradingview.db') as con:
     db = con.cursor()
-    db.execute("CREATE TABLE IF NOT EXISTS tv (screener TEXT, exchange TEXT, symbol TEXT, desc TEXT)")
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS tv (screener TEXT, exchange TEXT, symbol TEXT, desc TEXT)")
     con.commit()
 
-screener = ["america", "forex", "crypto", "indonesia", "india", "cfd", "uk", "brazil", "vietnam", "rsa", "ksa", "australia", "russia", "thailand", "philippines", "taiwan", "sweden", "france", "turkey", "euronext", "germany", "spain"]
+# Delete all rows
+with sqlite3.connect('tradingview.db') as con:
+    db = con.cursor()
+    db.execute("DELETE FROM tv")
+    con.commit()
 
-def add(screener, exchange, symbol, description):
-    with sqlite3.connect('tradingview.db') as con:
-        db = con.cursor()
-        db.execute("INSERT INTO tv SELECT ?, ?, ?, ? WHERE NOT EXISTS (SELECT 1 FROM tv WHERE screener = ? AND exchange = ? AND symbol = ?)", (screener, exchange, symbol, description, screener, exchange, symbol))
-        con.commit()
+screener = ["america", "forex", "crypto", "indonesia", "india", "cfd", "uk", "brazil", "vietnam", "rsa", "ksa",
+            "australia", "russia", "thailand", "philippines", "taiwan", "sweden", "france", "turkey", "euronext", "germany", "spain"]
 
 for x in screener:
-    r = requests.post(f"https://scanner.tradingview.com/{x}/scan", data='{"symbols":{"tickers":[],"query":{"types":[]}},"columns":["description"]}')
+    r = requests.post(f"https://scanner.tradingview.com/{x}/scan",
+                      data='{"symbols":{"tickers":[],"query":{"types":[]}},"columns":["description"]}')
+    data = []
     for res in r.json()["data"]:
         exchange, symbol = res["s"].split(":")
         desc = res["d"][0]
-        add(x, exchange, symbol, desc)
+        data.append((x, exchange, symbol, desc,))
+
+    # Use bulk operation for faster insert
+    with sqlite3.connect('tradingview.db') as con:
+        con.executemany(
+            "INSERT INTO tv VALUES (?, ?, ?, ?)", 
+            data
+        )
