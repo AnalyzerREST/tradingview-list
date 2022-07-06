1562b2aea29ba2c07f0f8e231e0af6368fd8df5e
Brian <brian@brianthe.dev>
2022-07-06 19:36:31 +0700

    Add support for futures

--- update.py
+++ update.py
@@ -17,18 +17,20 @@ with sqlite3.connect('tradingview.db') as con:
 screener = ["america", "forex", "crypto", "indonesia", "india", "cfd", "uk", "brazil", "vietnam", "rsa", "ksa",
             "australia", "russia", "thailand", "philippines", "taiwan", "sweden", "france", "turkey", "euronext", "germany", "spain"]
 
-for x in screener:
-    r = requests.post(f"https://scanner.tradingview.com/{x}/scan",
-                      data='{"symbols":{"tickers":[],"query":{"types":[]}},"columns":["description"]}')
-    data = []
-    for res in r.json()["data"]:
-        exchange, symbol = res["s"].split(":")
-        desc = res["d"][0]
-        data.append((x, exchange, symbol, desc,))
+for types in ["", "\"futures\""]:
+    for x in screener:
+        print(f"Loading screener: {x}")
+        r = requests.post(f"https://scanner.tradingview.com/{x}/scan",
+                          data=f'{{"symbols":{{"tickers":[],"query":{{"types":[{types}]}}}},"columns":["description"]}}')
+        data = []
+        for res in r.json()["data"]:
+            exchange, symbol = res["s"].split(":")
+            desc = res["d"][0]
+            data.append((x, exchange, symbol, desc,))
 
-    # Use bulk operation for faster insert
-    with sqlite3.connect('tradingview.db') as con:
-        con.executemany(
-            "INSERT INTO tv VALUES (?, ?, ?, ?)", 
-            data
-        )
+        # Use bulk operation for faster insert
+        with sqlite3.connect('tradingview.db') as con:
+            con.executemany(
+                "INSERT INTO tv VALUES (?, ?, ?, ?)",
+                data
+            )
